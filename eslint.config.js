7b41e73315dea87985d40fbb3f7c5d66f1ebf294
wesleymessiassilva1907 <wesley.silva.messias@gmail.com>
2025-09-02 18:49:26 -0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,11 @@
-
 export default [
-  {
-    ignores: [
-      "node_modules/**",
-      "dist/**",
-      "coverage/**",
-      ".github/**",
-      "eslint.config.*"
-    ]
-  },
-
+  { ignores: ["node_modules/**","dist/**","coverage/**",".github/**","eslint.config.*"] },
   {
     files: ["**/*.js"],
     languageOptions: {
       ecmaVersion: 2023,
-      sourceType: "module",
-      globals: {
-        process: "readonly",
-        __dirname: "readonly",
-        module: "readonly",
-        require: "readonly",
-        console: "readonly"
-      }
+      sourceType: "module", // <- importante
+      globals: { process:"readonly", console:"readonly" }
     },
     rules: {
       "no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
